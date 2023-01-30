da891e2071a1cbd37f33e99f9459476d7f86ebde
start.vaadin.com <no-reply@vaadin.com>
2023-01-30 11:13:21 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/palaute/PalauteView.java
@@ -0,0 +1,33 @@
+package com.example.application.views.palaute;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Palaute")
+@Route(value = "palaute", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class PalauteView extends VerticalLayout {
+
+    public PalauteView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
