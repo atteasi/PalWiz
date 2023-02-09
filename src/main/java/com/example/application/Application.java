4968221c9b794e926e4e226bf59c3f19ada71110
Pall00 <juhoahol@gmail.com>
2023-02-09 16:11:25 +0200

    Ulkoasu muutettu tummaksi ja palaute sivulla testissä joku valmis pohja

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -5,6 +5,7 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 import javax.sql.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,7 +21,8 @@ import org.springframework.context.annotation.Bean;
  *
  */
 @SpringBootApplication
-@Theme(value = "myapp")
+
+@Theme(value = "myapp", variant = Lumo.DARK)
 @Push
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 @NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
