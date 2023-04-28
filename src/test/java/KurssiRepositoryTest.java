b7ce764abd514eed3c17c451a8470561a49ff7ff
Pall00 <juhoahol@gmail.com>
2023-04-28 13:30:35 +0300

    testejä

--- src/test/java/KurssiRepositoryTest.java
+++ src/test/java/KurssiRepositoryTest.java
@@ -1,96 +1,40 @@
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
-import java.sql.Date;
-import java.sql.Time;
-import java.time.LocalDate;
-
-import java.util.List;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import com.example.application.Application;
 
 import com.example.application.data.entity.Kurssi;
-import com.example.application.data.entity.User;
 import com.example.application.data.service.KurssiRepository;
-import com.example.application.data.service.KurssiService;
-import com.example.application.data.service.UserService;
 
-@RunWith(SpringRunner.class)
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-@SpringBootTest(classes = Application.class)
+import java.sql.Date;
+import java.util.Arrays;
+import java.util.List;
 
-@AutoConfigureTestDatabase
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
 public class KurssiRepositoryTest {
 
-    @Autowired
+    @Mock
     private KurssiRepository kurssiRepository;
-
-    @Autowired
-    private KurssiService kurssiService;
-
-    @Autowired
-    private UserService userService;
-
-    private Kurssi kurssi1;
-    private Kurssi kurssi2;
-    private User user;
-
-    @Before
-    public void setUp() {
-        User user1 = new User("testaaja", "tauvo", "test", "salasana", null);
-        user = userService.update(user1);
-
-        kurssi1 = new Kurssi("Ohjelmointi 1", "OP1", Date.valueOf(LocalDate.of(2022, 1, 1)),
-                Date.valueOf(LocalDate.of(2022, 12, 31)), user);
-
-        kurssiService.saveKurssi(kurssi1);
-
-        kurssi2 = new Kurssi("Ohjelmointi 2", "OP2", Date.valueOf(LocalDate.of(2022, 1, 1)),
-                Date.valueOf(LocalDate.of(2022, 12, 31)),
-                user);
-
-        kurssiService.saveKurssi(kurssi2);
-    }
-
-    @After
-    public void tearDown() {
-        kurssiRepository.deleteAll();
-    }
 
     @Test
     public void testFindKurssiById() {
-        Kurssi kurssi = kurssiService.findKurssi(kurssi1.getId());
-        assertNotNull(kurssi);
-        assertEquals(kurssi1.getNimi(), kurssi.getNimi());
+        // Arrange
+        int id = 1;
+        Kurssi mockKurssi = new Kurssi();
+        when(kurssiRepository.findKurssiById(id)).thenReturn(mockKurssi);
+
+        // Act
+        Kurssi result = kurssiRepository.findKurssiById(id);
+
+        // Assert
+        verify(kurssiRepository, times(1)).findKurssiById(id);
+        // Add other assertions as necessary
     }
 
-    /*
-    @Test
-    public void testFindKurssiByUserId() {
-        List<Kurssi> kurssit = kurssiService.findUserKurssit(kurssi1.getUser().getId());
-        assertNotNull(kurssit);
-        assertEquals(2, kurssit.size());
-        assertEquals(kurssi1.getNimi(), kurssit.get(0).getNimi());
-
-    }
-    */
-    @Test
-    public void testUpdateKurssi() {
-        kurssi1.setNimi("Ohjelmointi 3");
-        kurssiService.muokkaaKurssia(kurssi1);
-        Kurssi kurssi = kurssiService.findKurssi(kurssi1.getId());
-        assertEquals("Ohjelmointi 3", kurssi.getNimi());
-
-    }
-
+    // Add other test methods as necessary
 }
